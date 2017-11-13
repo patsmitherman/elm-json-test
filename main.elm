50f5e7aad90b15615154b340fce7325d6155acbc
Patrick Smitherman <patrick@Patricks-MacBook-Pro.local>
2017-11-12 19:20:19 -0600

    Initial commit

--- main.elm
+++ main.elm
@@ -1,6 +1,10 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
 
 
 main : Program Never Model Msg
@@ -15,33 +19,59 @@ main =
 
 type alias Model =
     { name : String
+    , url : String
+    , loading : Bool
     }
 
 
 modelInitialValue : Model
 modelInitialValue =
-    { name = "Test" }
+    { name = "Test"
+    , url = "qweqwe"
+    , loading = False
+    }
 
 
 type Msg
-    = Msg1
-    | Msg2
+    = NewImage (Result Http.Error String)
+    | LoadImage String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Msg1 ->
-            ( model, Cmd.none )
+        NewImage (Ok newUrl) ->
+            ( { model | url = newUrl, loading = False }, Cmd.none )
 
-        Msg2 ->
-            ( model, Cmd.none )
+        NewImage (Err _) ->
+            ( { model | loading = False }, Cmd.none )
+
+        LoadImage topic ->
+            ( { model | url = "./assets/loading.gif", loading = True }, getRandomGif topic )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ text "New Html Program"
+        [ div [ style [ ( "margin", "20px auto" ), ( "text-align", "center" ) ] ]
+            [ button
+                [ style
+                    [ ( "width", "150px" )
+                    , ( "height", "40px" )
+                    , ( "border-radius", "5px" )
+                    , ( "border", "solid 1px #999" )
+                    , ( "outline", "none" )
+                    ]
+                , onClick (LoadImage "puppy")
+                ]
+                [ text "Load Picture" ]
+            ]
+        , div [ style [ ( "margin", "auto 0" ), ( "text-align", "center" ), ( "min-width", "100px" ), ( "min-height", "100px" ) ] ]
+            [ if model.loading then
+                img [ src model.url, width 100, height 100 ] []
+              else
+                img [ src model.url ] []
+            ]
         ]
 
 
@@ -53,3 +83,32 @@ subscriptions model =
 init : ( Model, Cmd Msg )
 init =
     ( modelInitialValue, Cmd.none )
+
+
+
+-- HTTP
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+    let
+        url =
+            "https://dog.ceo/api/breed/boxer/images/random"
+
+        --"https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+    in
+        Http.send NewImage (Http.get url decodeGifUrl)
+
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+    Decode.at [ "message" ] Decode.string
+
+
+
+--Decode.at [ "data", "image_url" ] Decode.string
+{- {
+       status: "success",
+       message: "https://dog.ceo/api/img/vizsla/n02100583_2265.jpg"
+   }
+-}
